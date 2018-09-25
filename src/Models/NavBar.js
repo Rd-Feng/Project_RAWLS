8ed8d45d446c779462049bbb9dc36a1c30d93d0a
thenicopixie <391@holbertonschool.com>
2018-09-25 03:24:17 +0000

    included link for font-awesome icons

--- src/Models/NavBar.js
+++ src/Models/NavBar.js
@@ -6,6 +6,7 @@ class NavBar extends Component {
   render () {
     return (
       <div class="sidebar">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <a class="/" href="/"><i class="fa fa-fw fa-home"></i> Home</a>
         <a href="contract"><i class="fa fa-file"></i> Contracts</a>
         <a href=""><i class="fa fa-history"></i> History</a>
