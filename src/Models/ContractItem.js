6b6cc2639d0dccddd0454bf3d6bdb87e80ad60a9
thenicopixie <391@holbertonschool.com>
2018-09-24 22:15:44 -0700

    create contract accordions and toggle switches

--- src/Models/ContractItem.js
+++ src/Models/ContractItem.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
+import './styles.css'
 
 class ContractItem extends Component {
+	constructor(props) {
+    super(props)
+    this.state = {
+    	items: [
+      	{ text: "Amazon", open: false },
+        { text: "Fitbit", open: false },
+        { text: "Google", open: false }
+      ]
+    }
+  }
+  toggleState = (index) => {
+    var newItems = this.state.items;
+    newItems[index].open = !newItems[index].open;
+    this.setState({items: newItems})
+  }
 
   render () {
     return (
-      <div className="contractItem">
-        <h1>ContractItem place holder</h1>
-      </div>
-    );
+      <div className="main">
+        <ol>
+        {this.state.items.map((item, idx) => {
+        const accordionState = item.open ? 'active' : '';
+        const accordionClass = `accordion ${accordionState}`;
+        return (
+          <div  key={idx}>
+            <button onClick={() => {
+            this.toggleState(idx);}} className={accordionClass}>{item.text}</button>
+            <div className="panel">
+             <span>Age</span>
+                <label className="switch">
+                  <input type="checkbox"/>
+                  <span className="slider round"></span>
+                </label>
+                
+              <p>Gender
+                <label className="switch">
+                  <input type="checkbox"/>
+                  <span className="slider round"></span>
+                </label></p>
+              <p>Location
+                <label className="switch">
+                  <input type="checkbox"/>
+                  <span className="slider round"></span>
+                </label>
+               </p>
+              <button className="submit">Submit</button>
+              <button className="cancel">Cancel</button>
+            </div>            
+          </div>
+        )
+        })}
+          </ol>
+        </div>
+    )
   }
 }
 
