4ba568baf7e25a6c760d265cff74ef0d1db369d5
Rd-Feng <394@holbertonschool.com>
2018-09-28 11:04:46 -0700

    prepare for merging

--- src/Models/Homepage.js
+++ src/Models/Homepage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import YouTube from 'react-youtube'
 import './Homepage.css'
 
 class Homepage extends Component {
-
   render () {
     return (
       <div className="homepage">
-        <h1>Home page content</h1>
+        <h2>Earning Place Holder</h2><hr />
+        <h2>Wallet Info Place Holder</h2><hr />
+        <h2>Promotions Place Holder</h2><hr />
       </div>
     )
   }
