d66bf82106ee45cd6f84a913fdad013bbfb1a554
Bryan Leung <byn@Bryans-MacBook-Pro.local>
2018-09-30 20:55:50 -0700

    fixed all responsiveness of the navbar and its DOM elements

--- src/Models/NavBar.js
+++ src/Models/NavBar.js
@@ -8,10 +8,10 @@ class NavBar extends Component {
       <div className="sidebar">
         <img className="profile_pic" src={require('./profile_pic.jpeg')} />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-        <a href="/"><i className="fa fa-fw fa-home"></i> Home</a>
-        <a href="contract"><i className="fa fa-file"></i> Contracts</a>
-        <a href=""><i className="fa fa-history"></i> History</a>
-        <a href=""><i className="fa fa-question"></i> About</a>
+        <a href="/"><i className="navByn fa fa-fw fa-home"></i><span className="faText">Home</span></a>
+        <a href="contract"><i className="navByn fa fa-file"></i><span className="faText">Contracts</span></a>
+        <a href=""><i className="navByn fa fa-history"></i><span className="faText">History</span></a>
+        <a href=""><i className="navByn fa fa-question"></i><span className="faText">About</span></a>
       </div>
     )
   }
