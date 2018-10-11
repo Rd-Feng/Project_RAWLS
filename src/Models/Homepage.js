e2f57051ffc8d42fac2cd2366b98a7e60f131c67
Rd-Feng <394@holbertonschool.com>
2018-10-11 16:29:15 -0700

    cleanup

--- src/Models/Homepage.js
+++ src/Models/Homepage.js
@@ -10,18 +10,15 @@ import './Homepage.css'
               <div className="card">
 			         <div className="title">
 			            <h2 className="titleText">Current Contracts</h2></div>
-			               <p><b><a href="contract">Amazon</a></b></p>
-			               <p><b><a href="contract" >Fitbit</a></b></p>
-                     <p><b><a href="contract" >Google</a></b></p>
-                     <p>Some text..</p>
+			               <p><b>Amazon</b></p>
+			               <p><b>Fitbit</b></p>
+                     <p><b>Google</b></p>
+                     <a href="contract">See more...</a>
               </div>
               <div className="card">
 			         <div className="title">
 		             <h2 className="titleText">You can earn more...</h2></div>
-                      <h5>add page for new contract link</h5>
-                      <p><b><a href="contract" >new</a></b></p>
-                      <p><b><a href="contract" >new</a></b></p>
-                      <p><b><a href="contract" >new</a></b></p>
+                      <p><b><a href="contract" >View Contracts</a></b></p>
               </div>
             </div>
             <div className="rightColumn">
@@ -30,14 +27,12 @@ import './Homepage.css'
                   <h2 className="titleText">My Wallet</h2></div>
 			            <h3>balance $32</h3>
 			            <h3>Income $5</h3>
-                  <div className="fakeimg">wallet Image</div>
-                  <p>Some text </p>
               </div>
               <div className="card">
 			          <div className="title">
-			             <h2 className="titleText">title1</h2></div>
-			             <div className="fakeimg" style={{height: 200}}>Image</div>
-                   <p>Some text..</p>
+			             <h2 className="titleText">Follow Us</h2></div>
+                   <p>Twitter</p>
+                   <p>Facebook</p>
               </div>
               <div className="card">
 			           <div className="title">
