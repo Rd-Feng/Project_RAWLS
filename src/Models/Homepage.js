2d76fd99006a49830395e3bd2b215c6ef1a2e384
sumin3 <suminyu@yahoo.com>
2018-10-29 16:12:21 -0700

    add wallet page, margin for homepage

--- src/Models/Homepage.js
+++ src/Models/Homepage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import YouTube from 'react-youtube'
 import './Homepage.css'
 
 class Homepage extends Component {
@@ -29,50 +28,50 @@ class Homepage extends Component {
     contractItems = this.state.list.map(contract => {
       return (
         <div>
-          {contract.title}
+        {contract.title}
         </div>
       );
     })
     return (
       <div className="row">
-        <div className="leftColumn">
-          <div className="card">
-            <div className="title">
-              <h2 className="titleText">Current Contracts</h2></div>
-              <p><b>{contractItems}</b></p>
+      <div className="leftColumn">
+      <div className="card">
+      <div className="title">
+      <h2 className="titleText">Current Contracts</h2></div>
+      <p><b>{contractItems}</b></p>
 
-              <a href="contract">See more...</a>
-            </div>
-            <div className="card">
-              <div className="title">
-                <h2 className="titleText">You can earn more...</h2></div>
-                <p><b><a href="contract" >View Contracts</a></b></p>
-              </div>
-            </div>
+      <a href="contract">See more...</a>
+      </div>
+      <div className="card">
+      <div className="title">
+      <h2 className="titleText">You can earn more...</h2></div>
+      <p><b><a href="contract" >View Contracts</a></b></p>
+      </div>
+      </div>
 
-            <div className="rightColumn">
+      <div className="rightColumn">
 
-              <div className="card">
-                <div className="title">
-                  <h2 className="titleText">My Wallet</h2></div>
-                  <h3>balance $32</h3>
-                  <h3>Income $5</h3>
-                </div>
-                <div className="card">
-                  <div className="title">
-                    <h2 className="titleText">Follow Us</h2></div>
-                    <p>Twitter</p>
-                    <p>Facebook</p>
-                  </div>
-                  <div className="card">
-                    <div className="title">
-                      <h2 className="titleText">title3</h2></div>
-                      <p>Some text..</p>
-                    </div>
-                  </div>
-                </div>
-              )
-            }
-          }
+      <div className="card">
+      <div className="title">
+      <h2 className="titleText">My Wallet</h2></div>
+      <h3>balance $32</h3>
+      <h3>Income $5</h3>
+      </div>
+      <div className="card">
+      <div className="title">
+      <h2 className="titleText">Follow Us</h2></div>
+      <p>Twitter</p>
+      <p>Facebook</p>
+      </div>
+      <div className="card">
+      <div className="title">
+      <h2 className="titleText">title3</h2></div>
+      <p>Some text..</p>
+      </div>
+      </div>
+      </div>
+    )
+  }
+}
 
-          export default Homepage;
+export default Homepage;
