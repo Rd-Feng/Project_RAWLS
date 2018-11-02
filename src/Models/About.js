0f556f0d6bfdf4dae4cdb62ac83d9e2b6bdcc451
thenicopixie <391@holbertonschool.com>
2018-11-02 09:52:46 -0700

    Add about page contents - RAWLS, Mission, meet the team sections

--- src/Models/About.js
+++ src/Models/About.js
@@ -3,15 +3,35 @@ import './About.css'
 
 class About extends Component {
 
+
   render () {
     return(
     <div className="aboutContainer">
       <div className="aboutCard">
         <h1> RAWLS</h1>
-        <h2> Rawls is a platform empowering companies and
-        consumers to monetize personal data in a way that
-        fosters mutual trust and compensation, while overcoming legal
-        and business barriers.</h2>
+        <div className="aboutRawls">
+         <p> Rawls is a platform empowering companies and
+         consumers to monetize personal data in a way that
+         fosters mutual trust and compensation, while overcoming legal
+         and business barriers.</p>
+        </div>
+      </div>
+      <div className="aboutCard">
+       <h1> Mission </h1>
+       <p> To enable organizations to maximize on consumer data while compensating them fairly</p>
+       <p> To enable organizations to differentiate their brand based on the values of trust and integrity</p>
+       <p> To give consumers control and transparency on the privacy and monetary value of their data</p>
+      </div>
+      <div className="aboutCard">
+        <h1> Meet the Team </h1>
+        <br/>
+        <div className = "teamMembers">
+          <div><img className="person" src={require('./team/rui.JPG')} /><p>Rui</p></div>
+          <div><img className="person" src={require('./team/sumin.jpg')} /><p>Sumin</p></div> 
+          <div><img className="person" src={require('./team/bryan.jpeg')} /><p>Bryan</p></div>
+          <div><img className="person" src={require('./team/jian.png')} /><p>Jian</p></div>
+          <div><img className="person" src={require('./team/nicole.jpg')} /><p>Nicole</p></div>
+        </div>
       </div>
       <div className="contactUs">
       </div>
