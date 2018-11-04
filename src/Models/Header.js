bdfafcc7f014b2b0c71555860ec0389fa99a0cdb
sumin3 <suminyu@yahoo.com>
2018-11-04 14:54:46 -0800

    1. add if statement: not display header on login page 2. add a hoverable dropdown for sign out

--- src/Models/Header.js
+++ src/Models/Header.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import './Header.css'
 
 class Header extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {email: this.props.email}
+  }
   render () {
+    if(window.location.pathname==='/'){
+      return null;
+    }
     return (
+
       <div className="header">
+
+      <div class="dropdown">
+      <div className="dropbtn"> Hi, Username</div>
+      <div class="dropdown-content">
+      <a className="signOut left" href="/" >sign out</a>
+      </div>
+      </div>
       <img className="profile_pic" src={require('./userLogo.png')} />
+
       </div>
 
     )
