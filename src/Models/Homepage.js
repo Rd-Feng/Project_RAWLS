c739020bea536f1f145124aa04d56b97b123b344
sumin3 <suminyu@yahoo.com>
2018-11-04 15:04:11 -0800

    1: add block size for each block. 2: left and right column have the same width. 3: blod the title text. 4: add a div for text

--- src/Models/Homepage.js
+++ src/Models/Homepage.js
@@ -37,14 +37,13 @@ class Homepage extends Component {
       <div className="card">
       <div className="title">
       <h2 className="titleText">Current Contracts</h2></div>
-      <p><b>{contractItems}</b></p>
 
-      <a href="contract">See more...</a>
+      <a className="Text" href="contract">See more...</a>
       </div>
       <div className="card">
       <div className="title">
       <h2 className="titleText">You can earn more...</h2></div>
-      <p><b><a href="contract" >View Contracts</a></b></p>
+      <a className="Text" href="contract" >View Contracts</a>
       </div>
       </div>
 
@@ -53,20 +52,10 @@ class Homepage extends Component {
       <div className="card">
       <div className="title">
       <h2 className="titleText">My Wallet</h2></div>
-      <h3>balance $32</h3>
-      <h3>Income $5</h3>
+      <p className="Text">balance $32</p>
+      <p className="Text">Income $5</p>
       </div>
-      <div className="card">
-      <div className="title">
-      <h2 className="titleText">Follow Us</h2></div>
-      <p>Twitter</p>
-      <p>Facebook</p>
-      </div>
-      <div className="card">
-      <div className="title">
-      <h2 className="titleText">title3</h2></div>
-      <p>Some text..</p>
-      </div>
+
       </div>
       </div>
     )
