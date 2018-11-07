fdfe8d3305f6370758f8db1aa3ae418a937110eb
Rd-Feng <394@holbertonschool.com>
2018-11-07 11:28:42 -0800

    display first two contracts

--- src/Models/Homepage.js
+++ src/Models/Homepage.js
@@ -16,23 +16,8 @@ class Homepage extends Component {
     let { contractName } = contractInstance;
     contractName((err, name) => {
       if (err) console.error ('An error occured::::', err);
-      this.setState({title: name})
+      this.setState({title1: name})
     });
-    // if(this.props.contracts){
-    //   this.props.contracts.map(contract => {
-    //     const contractABI = window.web3.eth.contract(contract.abi)
-    //     const contractInstance = contractABI.at(contract.addr)
-    //     const { contractName } = contractInstance;
-    //     contractName((err, name) => {
-    //       if (err) console.error ('An error occured::::', err);
-    //       this.state.items.push(
-    //         {
-    //           title: name
-    //         }
-    //       )
-    //     });
-    //   })
-    // }
   }
   getContractName2 () {
     let contractABI = window.web3.eth.contract(this.props.contracts[1].abi)
@@ -44,48 +29,37 @@ class Homepage extends Component {
     });
   }
   render () {
-    // let contractItems;
-    // console.log(this.state.list)
-    // contractItems = this.state.list.map(contract => {
-    //   return (
-    //     <div key={contract.title}>
-    //     {contract.title}
-    //     </div>
-    //   );
-    // })
     return (
       <div className="row">
         <div className="leftColumn">
           <div className="card">
             <div className="title">
-              <h2 className="titleText">Current Contracts</h2></div>
-              <p className="Text">{this.state.title}</p>
-              <p className="Text">{this.state.title2}</p>
-              <a className="Text" href="contract">See more...</a>
+              <h2 className="titleText">Current Contracts</h2>
             </div>
-            <div className="card">
-              <div className="title">
-                <h2 className="titleText">You can earn more...</h2>
-              </div>
-              <p className="Text">{this.state.title}</p>
-              <a className="Text" href="contract" >View Contracts</a>
+            <p className="Text">{this.state.title1}</p>
+            <p className="Text">{this.state.title2}</p>
+            <a className="Text" href="contract">See more...</a>
+          </div>
+          <div className="card">
+            <div className="title">
+              <h2 className="titleText">You can earn more...</h2>
+            </div>
+            <p className="Text">{this.state.title1}</p>
+            <a className="Text" href="contract" >View Contracts</a>
+          </div>
+        </div>
+        <div className="rightColumn">
+          <div className="card">
+            <div className="title">
+              <h2 className="titleText">My Wallet</h2></div>
+              <p className="Text">Balance $32</p>
+              <p className="Text">Monthly Income $5</p>
+              <a className="Text" href="wallet">See Transactions...</a>
             </div>
           </div>
-
-          <div className="rightColumn">
-
-            <div className="card">
-              <div className="title">
-                <h2 className="titleText">My Wallet</h2></div>
-                <p className="Text">Balance $32</p>
-                <p className="Text">Monthly Income $5</p>
-                <a className="Text" href="wallet">See Transactions...</a>
-              </div>
-
-            </div>
-          </div>
-        )
-      }
+        </div>
+      )
     }
+  }
 
-    export default Homepage;
+  export default Homepage;
