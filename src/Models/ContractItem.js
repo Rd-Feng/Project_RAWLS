b50d44262846738357f7d33696a47a99bb83066a
sumin3 <suminyu@yahoo.com>
2018-11-16 10:24:11 -0800

    add info hover dropdown

--- src/Models/ContractItem.js
+++ src/Models/ContractItem.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { Collapse, Button, CardBody, Card, CardTitle, CardText  } from 'reactstrap'
+import { Collapse, Button, CardBody, Card, CardTitle, CardText } from 'reactstrap'
 import './ContractItem.css'
 
 class ContractItem extends Component {
-	constructor (props) {
+	constructor(props) {
 		super(props)
 		this.permRefs = []
 		this.l = []
@@ -14,25 +14,25 @@ class ContractItem extends Component {
 			perms: []
 		}
 	}
-	componentDidMount () {
+	componentDidMount() {
 		this.getContractName();
 		this.getPerms();
 		this.getPayment();
 		this.getTotal();
 	}
-	getPerms () {
+	getPerms() {
 		const contractABI = window.web3.eth.contract(this.props.contract.abi)
 		const contractInstance = contractABI.at(this.props.contract.addr)
 		const { numPerms } = contractInstance;
 		numPerms((err, permsCount) => {
-			if (err) console.error ('An error occured::::', err);
+			if (err) console.error('An error occured::::', err);
 			const contractABI = window.web3.eth.contract(this.props.contract.abi)
 			const contractInstance = contractABI.at(this.props.contract.addr)
 			let i;
-			for (i = 0; i < permsCount; i++){
+			for (i = 0; i < permsCount; i++) {
 				let { getPerms } = contractInstance;
 				getPerms(i, (err, perm) => {
-					if (err) console.error ('An error occured::::', err);
+					if (err) console.error('An error occured::::', err);
 					let r = React.createRef();
 					this.permRefs.push(r)
 					this.l.push({
@@ -56,49 +56,48 @@ class ContractItem extends Component {
 				})
 			}
 		});
-		this.setState({perms: this.l})
+		this.setState({ perms: this.l })
 	}
-	getContractName () {
+	getContractName() {
 		const contractABI = window.web3.eth.contract(this.props.contract.abi)
 		const contractInstance = contractABI.at(this.props.contract.addr)
 		const { contractName } = contractInstance;
 		contractName((err, name) => {
-			if (err) console.error ('An error occured::::', err);
-			this.setState({title: name})
+			if (err) console.error('An error occured::::', err);
+			this.setState({ title: name })
 		});
 	}
-	getPayment () {
+	getPayment() {
 		const contractABI = window.web3.eth.contract(this.props.contract.abi)
 		const contractInstance = contractABI.at(this.props.contract.addr)
 		const { payment } = contractInstance;
 		payment((err, p) => {
-			if (err) console.error ('An error occured::::', err);
+			if (err) console.error('An error occured::::', err);
 			// console.log('payment::: ', p['c'][0])
-			this.setState({payment: p['c'][0]})
+			this.setState({ payment: p['c'][0] })
 		});
 	}
-	getTotal () {
+	getTotal() {
 		const contractABI = window.web3.eth.contract(this.props.contract.abi)
 		const contractInstance = contractABI.at(this.props.contract.addr)
 		const { total } = contractInstance;
 		total((err, t) => {
-			if (err) console.error ('An error occured::::', err);
+			if (err) console.error('An error occured::::', err);
 			// console.log('Total::: ', t['c'][0])
-			this.setState({total: t['c'][0]})
+			this.setState({ total: t['c'][0] })
 		});
 	}
-	togglePanel () {
+	togglePanel() {
 		let news = !this.state.show_panel
-		this.setState({show_panel: news})
+		this.setState({ show_panel: news })
 	}
-	handleSubmit () {
+	handleSubmit() {
 		const contractABI = window.web3.eth.contract(this.props.contract.abi)
 		const contractInstance = contractABI.at(this.props.contract.addr)
 		const { changeState } = contractInstance;
 		let changed = false;
-		this.state.perms.forEach(function(perm) {
-			if (perm.changed)
-			{
+		this.state.perms.forEach(function (perm) {
+			if (perm.changed) {
 				changed = true;
 				let newState = perm.reference.current.checked
 				changeState(perm.idx.c[0], newState, (err) => {
@@ -110,7 +109,7 @@ class ContractItem extends Component {
 		});
 		if (changed) alert('Your changes will take effect after around 1 min');
 	}
-	render () {
+	render() {
 		const accordionState = this.state.show_panel ? 'active' : '';
 		const accordionClass = `accordion ${accordionState}`;
 		let permissions;
@@ -118,33 +117,39 @@ class ContractItem extends Component {
 			return (
 				<div key={perm.title}>
 					<p className="contractText">{perm.title}
-					<span className="PermPrice"> {'\u00A0'}${perm.price}</span>
+						<span className="PermPrice"> {'\u00A0'}${perm.price}</span>
+						<div className="info_dp_contract">
+							<i className="fa fa-fw fa-info info_contract"></i>
+							<div className="info_dp-content_contract">
+								<p className="infoTextContract">PermPrice info</p>
+							</div>
+						</div>
 						<label className="switch">
 							<input
 								ref={perm.reference}
 								type="checkbox"
 								defaultChecked={perm.perm ? 'checked' : ''}
-								onClick={() => {perm.changed = !perm.changed}}
+								onClick={() => { perm.changed = !perm.changed }}
 							/>
 							<span className="slider round"></span>
 						</label></p>
-						<hr />
-					</div>
-				);
-			})
-			return (
-				<div>
-					<button onClick={() => {this.togglePanel();}}
-						className={accordionClass}>{this.state.title}
-					</button>
-					<div className="panel">
-						{permissions}
-						<p className="Total"> Total: ${this.state.payment} </p>
-						<button className="closeButton" onClick={() => {this.togglePanel();}}>
-							Close
+					<hr />
+				</div>
+			);
+		})
+		return (
+			<div>
+				<button onClick={() => { this.togglePanel(); }}
+					className={accordionClass}>{this.state.title}
+				</button>
+				<div className="panel">
+					{permissions}
+					<p className="Total"> Total: ${this.state.payment} </p>
+					<button className="closeButton" onClick={() => { this.togglePanel(); }}>
+						Close
 						</button>
-						<button className="submitButton right" onClick={() => {this.handleSubmit();}}>
-							Submit
+					<button className="submitButton right" onClick={() => { this.handleSubmit(); }}>
+						Submit
 						</button>
 				</div>
 			</div>
