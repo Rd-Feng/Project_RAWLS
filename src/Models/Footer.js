9d4b23cf4179ff8193c533ad684456bda6f59273
sumin3 <suminyu@yahoo.com>
2018-12-11 16:59:02 -0800

    add footer style, use footer tag for render

--- src/Models/Footer.js
+++ src/Models/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
 
   render () {
     return (
-      <div className="footer">
-        <h1>This is a footer</h1>
-      </div>
+      <footer className="footer">
+        RAWLS
+      </footer>
     )
   }
 }
