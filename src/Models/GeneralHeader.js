343f8a72d6ac3f0d57dad6b068ef9e691d6f3f02
sumin3 <suminyu@yahoo.com>
2018-12-13 12:03:04 -0800

    init commit

---
+++ src/Models/GeneralHeader.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import './styles/GeneralHeader.css'
+import Login from './Login'
+import Signup from './Signup'
+
+class GeneralHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: this.props.email,
+      home: this.props.active == "home" ? 'current' : '',
+      contract: this.props.active == "contract" ? 'current' : '',
+      wallet: this.props.active == "wallet" ? 'current' : '',
+      about: this.props.active == "about" ? 'current' : '',
+      logout: this.props.active == "logout" ? 'current' : ''
+    }
+  }
+  render() {
+    if (window.location.pathname === '/Signup') {
+      return null;
+    }
+    return (
+      <div className="body">
+        <nav className="navbar_main">
+          <div className="container">
+            <div className="company_name">
+              <h2>RAWLS</h2>
+            </div>
+            <div className="navbar_collapse">
+              <ul className="nav_item_container">
+                <li className="navbar_item">
+                  <a className="header_button" href="#sign_in_popup">sign in</a>
+                </li>
+                <li className="navbar_item">
+                  <a className="header_button" href="#sign_up_popup">sign up</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div id="sign_in_popup" class="sign_in_overlay">
+            <div class="signIn_popup">
+              <a class="Gclose" href="#">&times;</a>
+              <Login />
+            </div>
+          </div>
+
+          <div id="sign_up_popup" class="sign_in_overlay">
+            <div class="signIn_popup">
+              <a class="Gclose" href="#">&times;</a>
+              <Signup />
+            </div>
+          </div>
+
+        </nav>
+
+        <header className="Gheader_main">
+          <div className="Gheader_content">
+            <div className="Gheader_container">
+              <h2>Share Your Data</h2>
+              <h3>To Earn Money Every Month</h3>
+            </div>
+          </div>
+        </header>
+
+      </div>
+    )
+  }
+}
+
+export default GeneralHeader;
