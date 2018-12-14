e92c2f78af60aa9afa0be9a601986eb3b7da770c
Rd-Feng <394@holbertonschool.com>
2018-12-14 14:34:35 -0800

    update: use hint for login error message

--- src/Models/Login.js
+++ src/Models/Login.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Route, withRouter } from 'react-router-dom';
 import {
-  ButtonToolbar,
-  ButtonGroup,
-  Button, FormGroup,
-  FormControl,
-  ControlLabel
+	ButtonToolbar,
+	ButtonGroup,
+	Button, FormGroup,
+	FormControl,
+	ControlLabel
 } from "react-bootstrap";
 import fire from './fire'
 import "./styles/Login.css";
@@ -13,121 +13,125 @@ import "./styles/Login.css";
 const UsernameContext = React.createContext();
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.state = {
-      email: '',
-      password: '',
-      correctuname: '',
-      correctpasswd: ''
-    };
-
-  }
-
-  queryDatabase(uname) {
-    var db = fire.database();
-    var ref = db.ref('Users');
-
-    db.ref('Users').child(uname).once('value').then(
-      data => {
-        this.setState({
-          correctpasswd: data.val().Password,
-          correctuname: this.state.email
-        }, () => {
-          this.handleSubmit();
-        })
-      }
-    ).catch(err => {this.handleSubmit();});
-  }
-
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
-  componentWillMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  handleKeyPress(event) {
-    if (event.keyCode != 13) return;
-    // this.handleSubmit();
-    this.queryDatabase(this.state.email);
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
-  handleSubmit() {
-	if (this.state.email != this.state.correctuname)
-	{
-		alert("Username not found. Try again or register for new user.")
+	constructor(props) {
+		super(props);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.state = {
+			email: '',
+			password: '',
+			correctuname: '',
+			correctpasswd: '',
+			err_msg: ''
+		}
 	}
-	else if (this.state.password != this.state.correctpasswd)
-	{
-		alert("Incorrect password.")
-	}
-    else
-	{
-      this.props.history.push('/homepage')
-    }
-  }
 
-  render() {
-    return (
-      <div className="Login">
-        <form onSubmit={this.registerUser}>
-        <div className="login_intro">
-        <h3>RAWLS</h3>
-        <p>Giving control and transparency on the privacy and monetary value of your data.</p>
-        </div>
-        <div className="login_content">
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel> Username </ControlLabel>
-            <FormControl
-              autoFocus
-              type="text"
-              value={this.state.email}
-              onChange={this.handleChange}
-              inputRef={ref => { this.inputEmail = ref }}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel> Password </ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-              inputRef={ref => { this.inputPassword = ref }}
-            />
-          </FormGroup>
-          <div>
-            <ButtonToolbar >
-              <div className='buttonDIV'>
-                <ButtonGroup className="touch">
-                  <Button
-                    bsStyle="info"
-                    disabled={!this.validateForm()}
-                    onClick={() => {
-                      this.queryDatabase(this.state.email);
-                    }}
-                  >
-                    Login
-                    </Button>
-                </ButtonGroup>
-              </div>
-            </ButtonToolbar>
-          </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-export default withRouter (Login);
+	queryDatabase(uname) {
+		var db = fire.database();
+		var ref = db.ref('Users');
+
+		if (this.state.email == '' || this.state.password == '') {
+			this.setState({err_msg: "username or password missing"});
+			return;
+		}
+		db.ref('Users').child(uname).once('value').then(
+			data => {
+				if (data.val()) {
+					this.setState({
+						correctpasswd: data.val().Password,
+						correctuname: data.val().Username,
+						err_msg: "logging in"
+					}, () => {
+						this.handleSubmit();
+					})
+				}
+				else {
+					this.setState({
+						correctpasswd: '',
+						correctuname: '',
+						err_msg: ""
+					}, () => {
+						this.handleSubmit();
+					});
+				}
+			}
+		).catch(err => {console.log(err)});
+	}
+	componentWillMount() {
+		document.addEventListener('keydown', this.handleKeyPress);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleKeyPress);
+	}
+
+	handleKeyPress(event) {
+		if (event.keyCode != 13) return;
+		this.queryDatabase(this.state.email);
+	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value,
+		});
+	}
+	handleSubmit() {
+		if (this.state.correctuname == '')
+			this.setState({err_msg: 'username not found'});
+		else if (this.state.password == this.state.correctpasswd)
+			this.props.history.push('/homepage');
+		else
+			this.setState({err_msg: 'incorrect password'})
+	}
+
+	render() {
+		return (
+			<div className="Login">
+				<form onSubmit={this.registerUser}>
+					<div className="login_intro">
+						<h3>RAWLS</h3>
+						<p>Giving control and transparency on the privacy and monetary value of your data.</p>
+					</div>
+					<div className="login_content">
+						<FormGroup controlId="email" bsSize="large">
+							<ControlLabel> Username </ControlLabel>
+							<FormControl
+								autoFocus
+								type="text"
+								onChange={this.handleChange}
+								inputRef={ref => { this.inputEmail = ref }}
+							/>
+						</FormGroup>
+						<FormGroup controlId="password" bsSize="large">
+							<ControlLabel> Password </ControlLabel>
+							<FormControl
+								type="password"
+								onChange={this.handleChange}
+								inputRef={ref => { this.inputPassword = ref }}
+							/>
+						</FormGroup>
+						<div>
+							<div className="errormsg">
+								<p>{this.state.err_msg}</p>
+							</div>
+							<ButtonToolbar >
+								<div className='buttonDIV'>
+									<ButtonGroup className="touch">
+										<Button
+											bsStyle="info"
+											onClick={() => {
+												this.queryDatabase(this.state.email);
+											}}
+											>
+												Login
+											</Button>
+										</ButtonGroup>
+									</div>
+								</ButtonToolbar>
+							</div>
+						</div>
+					</form>
+				</div>
+			);
+		}
+	}
+	export default withRouter (Login);
