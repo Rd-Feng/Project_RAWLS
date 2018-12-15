08cc7b90dc8c6c95cc64184678d1420771872353
Jian <jian.huang.8581@gmail.com>
2018-12-15 12:39:12 -0800

    update wallet page with new UI design

--- src/Models/Wallet.js
+++ src/Models/Wallet.js
@@ -3,76 +3,100 @@ import './styles/Wallet.css'
 class Wallet extends Component {
   render(){
     return (
-        <div className="wallet-container">
-            <div className="trans-label">
-                <p> Transaction History </p>
+      <div className="background-container">
+        <div className="transaction-container">
+          <div className="balance-container">
+            <br></br>
+            <div className="balance">
+              <p> Balance of this account</p>
+              <p className="balance-amount"> $ <b>73</b>.00 </p>
+              <p className="redeemed"> Amount redeemed: $2.00 </p>
             </div>
-            <div className="trans-history">
-                <div className="table-labels">
-                    <span> Date </span>
-                    <span> Company </span>
-                    <span> Type </span>
-                    <span> Amount </span>
-                </div>
-                <hr className="main-hr"/>
-                <div className="transaction-list">
-                    <div className="transaction">
-                        <p>    June 6, 2018  </p>
-                        <p>    Google      </p>
-                        <p>    Deposit    </p>
-                        <p>    + $1.00     </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    June 6, 2018   </p>
-                        <p>    Fitbit        </p>
-                        <p>    Deposit    </p>
-                        <p>    + $3.00     </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    June 25, 2018</p>
-                        <p>    Amazon    </p>
-                        <p>    Deposit    </p>
-                        <p>    + $3.00 </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    July 2, 2018 </p>
-                        <p>         ---         </p>
-                        <p>  Withdrawal</p>
-                        <p>     - $7.00 </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    July 6, 2018  </p>
-                        <p>    Google      </p>
-                        <p>    Deposit    </p>
-                        <p>    + $1.00     </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    July 6, 2018   </p>
-                        <p>    Fitbit        </p>
-                        <p>    Deposit    </p>
-                        <p>    + $3.00     </p>
-                    </div>
-                    <hr className="sub-hr"/>
-                    <div className="transaction">
-                        <p>    July 25, 2018</p>
-                        <p>    Amazon    </p>
-                        <p>    Deposit    </p>
-                        <p>    + $3.00 </p>
-                    </div>
-                </div>
+            <br></br>
+            <div className="current-contracts">
+              <p> Number of active contracts: 3</p>
+              <p> Number of inactive contracts: 0 </p>
             </div>
-            <hr className="main-hr"/>
-            <div className="wallet-balance">
-                <p> Balance </p>
-                <p> $ 7.00 </p>
+            <br></br>
+            <br></br>
+            <div className="redemption">
+              <button> Redeem </button>
             </div>
-            <br/>
+          </div>
+          <div className="transaction-history">
+            <br></br>
+            <div className="history-label">
+              <p> ID# </p>
+              <p> Date </p>
+              <p> Company </p>
+              <p> Type </p>
+              <p> Amount </p>
+            </div>
+            <hr/>
+            <div className="transaction">
+              <p> 9009 </p>
+              <p> July 20, 2018 </p>
+              <p>
+                <img src={require(`./images/Amazon Prime.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$4.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9008 </p>
+              <p> July 19, 2018 </p>
+              <p>
+                <img src={require(`./images/Fitbit.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$3.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9007 </p>
+              <p> July 15, 2018 </p>
+              <p>
+                <img src={require(`./images/Google Map.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$1.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9006 </p>
+              <p> June 29, 2018 </p>
+              <p> --- </p>
+              <p> Withdrawal </p>
+              <p className="Withdrawal-Amount"> -$25.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9005 </p>
+              <p> June 20, 2018 </p>
+              <p>
+                <img src={require(`./images/Amazon Prime.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$4.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9004 </p>
+              <p> June 19, 2018 </p>
+              <p>
+                <img src={require(`./images/Fitbit.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$3.00 </p>
+            </div>
+            <div className="transaction">
+              <p> 9003 </p>
+              <p> June 15, 2018 </p>
+              <p>
+                <img src={require(`./images/Google Map.png`)}/>
+              </p>
+              <p> Deposit </p>
+              <p className="Deposit-Amount"> +$1.00 </p>
+            </div>
+          </div>
         </div>
+      </div>
     )
   }
 }
