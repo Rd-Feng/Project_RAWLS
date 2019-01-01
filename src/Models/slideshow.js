13b0e9377f242ead2b60b517b2d3acfa30d83517
sumin3 <suminyu@yahoo.com>
2019-01-01 02:30:59 -0800

    change slogan, banner

--- src/Models/slideshow.js
+++ src/Models/slideshow.js
@@ -4,24 +4,24 @@ import './styles/slideshow.css'
 
 const items = [
     {
-      src: 'https://cdn.pixabay.com/photo/2018/08/16/11/36/mentor-3610255_1280.jpg',
+      src: 'https://cdn.pixabay.com/photo/2018/12/29/16/13/mountains-3901729_1280.jpg',
       altText: 'Slide 1',
-      caption: 'Earn Money Every Month',
+      caption: 'Earn Extra Money Every Month',
       header: 'Share Your Data',
       Text: "new" 
 
     },
     {
-      src: 'https://i.imgur.com/gnyx1Nl.jpg',
+      src: 'https://cdn.pixabay.com/photo/2016/07/30/19/47/banner-1557841_1280.jpg',
       altText: 'Slide 2',
-      caption: 'Slide 2',
-      header: 'Slide 2 Header'
+      caption: 'Share and Cancel at Anytime',
+      header: 'Control Your Data'
     },
     {
-      src: 'https://cdn.pixabay.com/photo/2018/01/24/18/05/background-3104413_1280.jpg',
+      src: 'https://cdn.pixabay.com/photo/2016/07/30/19/47/banner-1557842_1280.jpg',
       altText: 'Slide 3',
-      caption: 'Slide 3',
-      header: 'Slide 3 Header'
+      caption: 'Sell Your Data Directly and Transparently',
+      header: 'No Third Party'
     }
   ];
   
