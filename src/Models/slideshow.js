edfc879bcc21e694641785c78f5337e6e765c719
sumin3 <suminyu@yahoo.com>
2019-01-01 20:53:19 -0800

    change pic address

--- src/Models/slideshow.js
+++ src/Models/slideshow.js
@@ -4,7 +4,7 @@ import './styles/slideshow.css'
 
 const items = [
     {
-      src: 'https://cdn.pixabay.com/photo/2018/12/29/16/13/mountains-3901729_1280.jpg',
+      src: 'https://i.imgur.com/1QoqSRk.jpg',
       altText: 'Slide 1',
       caption: 'Earn Extra Money Every Month',
       header: 'Share Your Data',
@@ -12,13 +12,13 @@ const items = [
 
     },
     {
-      src: 'https://cdn.pixabay.com/photo/2016/07/30/19/47/banner-1557841_1280.jpg',
+      src: 'https://i.imgur.com/XNA3mSB.jpg',
       altText: 'Slide 2',
       caption: 'Share and Cancel at Anytime',
       header: 'Control Your Data'
     },
     {
-      src: 'https://cdn.pixabay.com/photo/2016/07/30/19/47/banner-1557842_1280.jpg',
+      src: 'https://i.imgur.com/ixBZ3lw.jpg',
       altText: 'Slide 3',
       caption: 'Sell Your Data Directly and Transparently',
       header: 'No Third Party'
