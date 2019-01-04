9a33ec06684fa1b19c530a9e6fd86781e699c2c4
sumin3 <suminyu@yahoo.com>
2019-01-04 13:34:45 -0800

    change background color, add link for extension

--- src/Models/generalHomepage.js
+++ src/Models/generalHomepage.js
@@ -5,103 +5,85 @@ class GeneralHomepage extends Component {
   render() {
     return (
       <div id="Ghompage_container">
-      <section id="how_to_start">
+        <section id="how_to_start">
           <h2>How To Get Started</h2>
           <div className="how_to_start_collapse">
 
-
-
-
-          <div className="how_to_start_container">
-          <div className="step5_pic"></div>
-            <div className="how_to_start_title">
-              <br></br>
-              <h3>Step 1</h3>
-              <p>
-                Install MetaMask (Chrome Extension) and create an account.
-                MetaMask is required for this site to function.
+            <div className="how_to_start_container">
+              <div className="step5_pic"></div>
+              <div className="how_to_start_title">
+                <br></br>
+                <h3>Step 1</h3>
+                <p>
+                  Install MetaMask (<a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en" target="_blank" rel="noopener noreferrer">Chrome</a> or <a href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/" target="_blank" rel="noopener noreferrer">Firefox </a>Extension) and create an account.
+                  MetaMask is required for this site to function.
               </p>
-              <p className="cheating"> FUCKEN </p>
-
+              </div>
             </div>
-          </div>
 
-
-
-
-
-
-          <div className="how_to_start_container">
-            <div className="step1_pic"></div>
-            <div className="how_to_start_title">
-              <br></br>
-              <h3>Step 2</h3>
-              <p>Create a free Rawls account and then sign in.
+            <div className="how_to_start_container">
+              <div className="step1_pic"></div>
+              <div className="how_to_start_title">
+                <br></br>
+                <h3>Step 2</h3>
+                <p>Create a free Rawls account and then sign in.
               </p>
-              <p className="cheating"> Im a </p>
+              </div>
             </div>
-          </div>
 
-
-
-
-
-          <div className="how_to_start_container">
-          <div className="step3_pic"></div>
-            <div className="how_to_start_title">
-              <br></br>
-              <h3>Step 3</h3>
-              <p>
-                Start earning!
-                Accept any contracts you choose to collect monthly earnings.
+            <div className="how_to_start_container">
+              <div className="step3_pic"></div>
+              <div className="how_to_start_title">
+                <br></br>
+                <h3>Step 3</h3>
+                <p>
+                  Start earning!
+                  Accept any contracts you choose to collect monthly earnings.
 
               </p>
-              <p className="cheating"> GENIUSSSS </p>
-
+              </div>
             </div>
-          </div>
 
-        <div className="how_to_start_container">
-        <div className="step4_pic"></div>
-            <div className="how_to_start_title">
-              <br></br>
-              <h3>Step 4</h3>
-              <p>
-                Redeem your monthly earnings.
-                View and accept more contracts to generate more passive income!
+            <div className="how_to_start_container">
+              <div className="step4_pic"></div>
+              <div className="how_to_start_title">
+                <br></br>
+                <h3>Step 4</h3>
+                <p>
+                  Redeem your monthly earnings.
+                  View and accept more contracts to generate more passive income!
               </p>
-              <p className="cheating"> SCREW CSS BRYAN IS A GOD </p>
 
+              </div>
             </div>
           </div>
-        </div>
 
         </section>
 
 
 
 
-      <section className="partnership_instruction">
-        <div className="partnership_text">
-          <h2>Our Partnerships</h2>
-          <p> Rawls partners are generous companies that use data to enhance their consumer experience.
-          Rawls enables you to earn money for services that you use on the daily.
-          You already give them your data when you use their products, so why not get some cash for it?
-          Here are some of our partners who want to be fair and transparent.
+        <section className="partnership_instruction">
+          <div className="partnership_text">
+            <h2>Our Partnerships</h2>
+            <p> Rawls partners are generous companies that use data to enhance their consumer experience.
+            Rawls enables you to earn money for services that you use on the daily.
+            You already give them your data when you use their products, so why not get some cash for it?
+            Here are some of our partners who want to be fair and transparent.
           </p>
-          <ul className="how_to_start_title">
-              <li><a href="https://www.google.com/">Google Map</a></li>
-              <li><a href="https://www.amazon.com/">Amazon Prime</a></li>
-              <li><a href="https://www.fitbit.com/home">Fitbit</a></li>
-              <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">And many more...</a></li>
+            <ul className="how_to_start_title">
+              <li><a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google Map</a></li>
+              <li><a href="https://www.amazon.com/" target="_blank" rel="noopener noreferrer">Amazon Prime</a></li>
+              <li><a href="https://www.fitbit.com/home" target="_blank" rel="noopener noreferrer">Fitbit</a></li>
+              <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">And many more...</a></li>
             </ul>
 
-        </div>
-        <div className="partnership_pic_container">
-          <img className="partnership_pic" alt="partnership" src={require('./images/partnership_logo.png')} />
-        </div>
-      </section>
-    </div >
+          </div>
+          <div className="partnership_pic_container">
+            <img className="partnership_pic" alt="partnership" src={require('./images/partnership_logo.png')} />
+          </div>
+        </section>
+      </div >
     )
   }
 }
